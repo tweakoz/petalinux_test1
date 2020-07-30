c2e9383a43936e9be5a438b356c27b29a7ec83a3
Michael T. Mayers <michael@tweakoz.com>
2020-07-29 20:22:06 -0700

    ..

--- build.py
+++ build.py
@@ -8,7 +8,6 @@ this_dir = path.Path(os.path.dirname(os.path.realpath(__file__)))
 vctx = vivado.Context(hostdir=this_dir,
                       FPGAPART="xczu7ev-fbvb900-1-i")
 
-dkey = str(this_dir/"hardware")
-dval = "/data/hardware"
-vctx.dirmaps[dkey]=dval
+vctx.dirmaps[str(this_dir/"hardware")]="/data/hardware"
+vctx.dirmaps[str(this_dir/"tftpboot")]="/tftpboot"
 vctx.shell_command(["./_build_image.sh"])
